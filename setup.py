b23cd3f62848e3812ddc11a703283d44f8f2987c
svandegar <svandegar@hotmail.com>
2019-04-01 14:16:12 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='hashchain',
+    version='0.1.0',
+    description='Helper to certify database entries by using hashchains',
+    long_description='',
+    url='https://github.com/svandegar/hashchain',
+    author='Seraphin Vandegar',
+    author_email='svandegar@hotmail.com',
+    license='MIT',
+    packages=['hashchain'],
+    zip_safe=False,
+    download_url='',
+    install_requires=[
+        '',
+        ''
+    ],
+    classifiers=[
+        'Development Status :: 3- Alpha',
+        'Licence :: MIT Licence',
+        'Programming Language :: Python 3.7'
+    ]
+
+)
