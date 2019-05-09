89a000a96292d70f6e770b118619a4332cfe3875
svandegar <svandegar@hotmail.com>
2019-05-09 16:03:26 -0700

    beta release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name='hashchain',
-    version='0.8.4',
+    version='1.0.0',
     description='Helper to certify database entries, using hashchains and blockchains',
     long_description=long_description,
     long_description_content_type="text/markdown",
